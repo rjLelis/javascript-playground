a08500f1d9e42d20ba001bed45f80eebf2d87b37
R.J Lelis <renatojlelis@gmail.com>
2018-06-24 11:20:53 -0300

    Minor changes in the file

--- palindrome/script.js
+++ palindrome/script.js
@@ -6,8 +6,8 @@ function palindrome(){
     const word = document.getElementById("word").value;
     const tested = document.getElementById("tested");
     if(word === reverseWord(word)){
-        tested.innerHTML = `The word ${word} is palindrome`;
+        tested.innerHTML = `The word '${word}' is palindrome`;
     } else{
-        tested.innerHTML = `The word ${word} isn't palindrome`;
+        tested.innerHTML = `The word '${word}' isn't palindrome`;
     }
 }
