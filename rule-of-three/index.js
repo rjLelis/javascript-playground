3e660592220e758794a8841583f7c30d5915312c
R.J Lelis <renatojlelis@gmail.com>
2019-03-07 22:30:26 -0300

    Changed function definitions to arrow functions

--- rule-of-three/index.js
+++ rule-of-three/index.js
@@ -1,24 +1,24 @@
-function calculate(){
+const calculate = () => {
     let ifNumber = document.getElementById("if").value;
     if(isEmpty(ifNumber)){
-        document.getElementById('result').innerHTML = 'That number is required for the calculation';
+        document.getElementById('result').innerHTML = 'The "if" can\'t be empty';
         return false;
     } else if(isNegative(ifNumber)){
-        document.getElementById('result').innerHTML = 'That number must positive for the calculation';
+        document.getElementById('result').innerHTML = 'The "if" number must positive';
         return false;
     }
     let isNumber = document.getElementById("is").value;
     let thenNumber = document.getElementById("then").value;
 
-    let result = (isNumber * thenNumber) /ifNumber;
+    let result = (isNumber * thenNumber) / ifNumber;
 
     document.getElementById("result").innerHTML = `${result}%`;
 }
 
-function isEmpty(number){
-    return number == '';
+const isEmpty = (number) => {
+    return number === '';
 }
 
-function isNegative(number){
+const isNegative = (number) =>  {
     return number <= 0;
 }
